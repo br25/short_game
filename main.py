f139443e4996292ecf9394ba28252fef9af44aa1
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-12-26 15:08:51 +0600

    Mario standup

--- main.py
+++ main.py
@@ -17,10 +17,21 @@ SCREEN = pygame.display.set_mode((800,800))
 pygame.display.set_caption("Jumping mario")
 
 
+
+# Jumping variable
+jumping = False
+
+# Mario's middle position
+X_POSITION, Y_POSITION = 400, 660
+
+
 # Images load and size defined
 STANDING_SURFACE = pygame.transform.scale(pygame.image.load("images/mario_standing.png"), (48, 64))
 JUMPING_SURFACE = pygame.transform.scale(pygame.image.load("images/mario_jumping.png"), (48, 64))
 BACKGROUND = pygame.image.load("images/background.png")
+
+# Rectangle around of Mario
+mario_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
 
 
 # Starting The game
@@ -30,11 +41,13 @@ while True:
             pygame.quit()
             sys.exit()
 
-
+    # Background picture set
     SCREEN.blit(BACKGROUND, (0,0))
+    # Mario standing
+    SCREEN.blit(STANDING_SURFACE, mario_rect)
 
     # pygame display updated
     pygame.display.update()
-    
+
     # Clock tick setup
     CLOCK.tick(60)
